1de974e6b57598621b745ba5262a24aaa207285e
TunmiDev <133062206+TunmiDev@users.noreply.github.com>
2025-06-06 05:20:36 +0100

    finished

--- script.js
+++ script.js
@@ -1,51 +1,79 @@
+let intervalId = setInterval(updateTime, 1000); // Auto-update for default cities
+let cityIntervalId; // For single city updates
+
 function updateTime() {
-  //Los Angeles Time Update
+  // Los Angeles Time Update
   let losAngelesElement = document.querySelector("#Los-Angeles");
-  let losAngelesDateElement = losAngelesElement.querySelector(".date");
-  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+  if (losAngelesElement) {
+    let losAngelesDateElement = losAngelesElement.querySelector(".date");
+    let losAngelesTimeElement = losAngelesElement.querySelector(".time");
 
-  let losAngelesTime = moment().tz("America/Los_Angeles");
+    let losAngelesTime = moment().tz("America/Los_Angeles");
 
-  losAngelesDateElement.innerHTML = losAngelesTime.format("dddd, MMMM Do YYYY");
-  losAngelesTimeElement.innerHTML = losAngelesTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
-  //Lagos Time Update
+    losAngelesDateElement.innerHTML =
+      losAngelesTime.format("dddd, MMMM Do YYYY");
+    losAngelesTimeElement.innerHTML = losAngelesTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
+
+  // Lagos Time Update
   let lagosElement = document.querySelector("#Lagos");
-  let lagosDateElement = lagosElement.querySelector(".date");
-  let lagosTimeElement = lagosElement.querySelector(".time");
+  if (lagosElement) {
+    let lagosDateElement = lagosElement.querySelector(".date");
+    let lagosTimeElement = lagosElement.querySelector(".time");
 
-  let lagosTime = moment().tz("Africa/Lagos");
+    let lagosTime = moment().tz("Africa/Lagos");
 
-  lagosDateElement.innerHTML = lagosTime.format("dddd, MMMM Do YYYY");
-  lagosTimeElement.innerHTML = lagosTime.format("h:mm:ss [<small>]A[</small>]");
+    lagosDateElement.innerHTML = lagosTime.format("dddd, MMMM Do YYYY");
+    lagosTimeElement.innerHTML = lagosTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 }
 
-updateTime();
-
-setInterval(updateTime, 1000);
-
 function updateCity(event) {
+  clearInterval(intervalId); // Stop default city updates
+  if (cityIntervalId) {
+    clearInterval(cityIntervalId); // Stop any previous city update
+  }
+
   let cityTimeZone = event.target.value;
   if (cityTimeZone === "current") {
     cityTimeZone = moment.tz.guess();
   }
 
-  let cityTime = moment().tz(cityTimeZone);
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
 
   let citiesElement = document.querySelector("#cities");
-
   citiesElement.innerHTML = `
     <div class="city">
       <div class="city-info">
         <h2>${cityName}</h2>
-        <div class="date">${cityTime.format("dddd, MMMM Do YYYY")}</div>
+        <div class="date"></div>
       </div>
-      <div class="time">${cityTime.format("h:mm:ss [<small>]A[</small>]")}</div>
+      <div class="time"></div>
     </div>
+    <a href="index.html" class="back-link">Back to all cities</a>
   `;
+
+  // Select newly created time/date elements
+  let cityDateElement = citiesElement.querySelector(".date");
+  let cityTimeElement = citiesElement.querySelector(".time");
+
+  // Update the time immediately
+  function updateSelectedCityTime() {
+    let cityTime = moment().tz(cityTimeZone);
+    cityDateElement.innerHTML = cityTime.format("dddd, MMMM Do YYYY");
+    cityTimeElement.innerHTML = cityTime.format("h:mm:ss [<small>]A[</small>]");
+  }
+
+  updateSelectedCityTime(); // Initial update
+  cityIntervalId = setInterval(updateSelectedCityTime, 1000); // Keep updating
 }
 
 let citiesSelectElement = document.querySelector("#city-select");
 citiesSelectElement.addEventListener("change", updateCity);
+
+// Initial update and interval start
+updateTime();
